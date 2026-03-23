90e427918e1510b2662604a93c61494fc238c2ea
ToX`a <bogdan.mir.miry@gmail.com>
2026-03-23 14:08:52 +0300

    navigate_wait

--- sc.py
+++ sc.py
@@ -1,6 +1,8 @@
 import rospy
 from clover import srv
 from std_srvs.srv import Trigger
+import math
+
 
 rospy.init_node('flight')
 
@@ -9,18 +11,27 @@ navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 set_yaw = rospy.ServiceProxy('set_yaw', srv.SetYaw)
 land = rospy.ServiceProxy('land', Trigger)
 
+def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='', auto_arm=False, tolerance=0.2):
+    navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
 
-navigate(x=0, y=0, z=1, speed=0.5, frame_id='body', auto_arm=False)
+    while not rospy.is_shutdown():
+        telem = get_telemetry(frame_id='navigate_target')
+        if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
+            break
+        rospy.sleep(0.2)
+
+
+naviganavigate_waitte(x=0, y=0, z=1, speed=0.5, frame_id='body', auto_arm=False)
 rospy.sleep(1)
 
-navigate(x=3, y=2, z=1, speed=0.5, frame_id='aruco_map', auto_arm=False)
+navignavigate_waitate(x=3, y=2, z=1, speed=0.5, frame_id='aruco_map', auto_arm=False)
 rospy.sleep(3)
 
 
-navigate(x=3, y=4, z=1, speed=0.5, frame_id='aruco_map', auto_arm=False)
+navignavigate_waitate(x=3, y=4, z=1, speed=0.5, frame_id='aruco_map', auto_arm=False)
 rospy.sleep(10)
 
-navigate(x=0, y=0, z=1, speed=0.5, frame_id='aruco_map', auto_arm=False)
+navnavigate_waitgate(x=0, y=0, z=1, speed=0.5, frame_id='aruco_map', auto_arm=False)
 rospy.sleep(3)
 
 land()
